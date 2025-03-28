365f065ffa651d0bd7e9d2a136e9720655b72a69
Nico Zuber <zuber.nico@gmail.com>
2025-03-28 10:50:03 +0100

    Added date and time writing.

--- src/McuPCF85263A.c
+++ src/McuPCF85263A.c
@@ -14,6 +14,7 @@
 
 #define McuPCF85263A_I2C_DEVICE_ADDRESS   (0x51) /* 7bit I2C address, not-shifted value */
 
+#pragma region RegisterMap
 /* RTC mode (default) register memory map */
 
 // RTC time and date
@@ -90,8 +91,9 @@
 #define McuPCF85263A_MEMORY_DATE_SIZE       (4) /* 0x04-0x07, number of bytes for date information on device */
 #define McuPCF85263A_MEMORY_TIME_DATE_SIZE  (8) /* 0x00-0x07, number of bytes for data and time information on device */
 
+#pragma endregion 
 
-
+#pragma region Typedefs
 typedef struct McuPCF85263A_TTIME {  /* Time in binary format (24h) */
   uint8_t hour;                      /* hours */
   uint8_t min;                       /* minutes */
@@ -108,6 +110,9 @@ typedef struct McuPCF85263A_TDATE {  /* Date in binary format */
   uint8_t dayOfWeek;                 /* Day of week, where 0 is the first day. In the range of 0..6 */
 } McuPCF85263A_TDATE;
 
+#pragma endregion
+
+#pragma region Register_ReadWrites
 uint8_t McuPCF85263A_ReadBlock(uint8_t addr, uint8_t *buf, size_t bufSize) {
   return McuGenericI2C_ReadAddress(McuPCF85263A_I2C_DEVICE_ADDRESS, &addr, sizeof(addr), buf, bufSize);
 }
@@ -124,9 +129,94 @@ uint8_t McuPCF85263A_WriteByte(uint8_t addr, uint8_t data) {
   return McuGenericI2C_WriteAddress(McuPCF85263A_I2C_DEVICE_ADDRESS, &addr, sizeof(addr), &data, sizeof(data));
 }
 
+
+uint8_t McuPCF85263A_ReadOffset(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_OFFSET, data);
+}
+uint8_t McuPCF85263A_WriteOffset(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_OFFSET, data);
+}
+
 uint8_t McuPCF85263A_ReadOscilator(uint8_t *data) {
   return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_OSCILATOR, data);
 }
+uint8_t McuPCF85263A_WriteOscilator(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_OSCILATOR, data);
+}
+
+uint8_t McuPCF85263A_ReadBatterySwitch(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_BATTERY_SWITCH, data);
+}
+uint8_t McuPCF85263A_WriteBatterySwitch(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_BATTERY_SWITCH, data);
+}
+
+uint8_t McuPCF85263A_ReadPinIO(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_PIN_IO, data);
+}
+uint8_t McuPCF85263A_WritePinIO(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_PIN_IO, data);
+}
+
+uint8_t McuPCF85263A_ReadFunction(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_Function, data);
+}
+uint8_t McuPCF85263A_WriteFunction(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_Function, data);
+}
+
+uint8_t McuPCF85263A_ReadINTAEnable(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_INTA_ENABLE, data);
+}
+uint8_t McuPCF85263A_WriteINTAEnable(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_INTA_ENABLE, data);
+}
+
+uint8_t McuPCF85263A_ReadINTBEnable(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_INTB_ENABLE, data);
+}
+uint8_t McuPCF85263A_WriteINTBEnable(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_INTB_ENABLE, data);
+}
+
+uint8_t McuPCF85263A_ReadFlags(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_FLAGS, data);
+}
+uint8_t McuPCF85263A_WriteFlags(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_FLAGS, data);
+}
+
+uint8_t McuPCF85263A_ReadRamByte(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_RAM_Byte, data);
+}
+uint8_t McuPCF85263A_WriteRamByte(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_RAM_Byte, data);
+}
+
+uint8_t McuPCF85263A_ReadWatchDog(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_WATCHDOG, data);
+}
+uint8_t McuPCF85263A_WriteWatchDog(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_WATCHDOG, data);
+}
+
+uint8_t McuPCF85263A_ReadStopEnable(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_STOP_ENABLE, data);
+}
+uint8_t McuPCF85263A_WriteStopEnable(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_STOP_ENABLE, data);
+}
+
+uint8_t McuPCF85263A_ReadResets(uint8_t *data) {
+  return McuPCF85263A_ReadByte(McuPCF85263A_ADDRESS_CONTROL_RESETS, data);
+}
+uint8_t McuPCF85263A_WriteResets(uint8_t data) {
+  return McuPCF85263A_WriteByte(McuPCF85263A_ADDRESS_CONTROL_RESETS, data);
+}
+
+#pragma endregion Register_ReadWrites
+
+#pragma region TimeDateFunctions
 
 bool McuPCF85263A_Is24hMode(void) {
   uint8_t res, osc;
@@ -165,6 +255,77 @@ uint8_t McuPCF85263A_ReadTimeDate(McuPCF85263A_TTIME *time, McuPCF85263A_TDATE *
   return ERR_OK;
 }
 
+uint8_t McuPCF85263A_WriteTimeDate(McuPCF85263A_TTIME *time, McuPCF85263A_TDATE *date) {
+  uint8_t buf[McuPCF85263A_MEMORY_TIME_DATE_SIZE];
+  if (   ((time->mode==McuPCF85263A_TTIME_MODE_12H)&&((time->hour>12)||(time->hour==0)))
+      || ((time->mode==McuPCF85263A_TTIME_MODE_24H)&&(time->hour>23))
+      || (time->min>59)||(time->sec>59)
+      || (date->year>99)||(date->month>12)||(date->month==0)
+      || (date->day>31)||(date->day==0)
+      || (date->dayOfWeek>6))
+  {
+    return ERR_RANGE;
+  }
+  buf[0] = (uint8_t)(((time->cs/10)<<4) | (time->cs%10));
+  buf[1] = (uint8_t)(((time->sec/10)<<4) | (time->sec%10));
+  buf[2] = (uint8_t)(((time->min/10)<<4) | (time->min%10));
+  buf[3] = (uint8_t)(((time->hour/10)<<4) | (time->hour%10));
+  if (time->mode==McuPCF85263A_TTIME_MODE_12H) {
+    buf[3] |= (time->am_pm)?(1<<5):0;
+  }
+  buf[4] = (uint8_t)(((date->day/10)<<4) | (date->day%10));
+  buf[5] = (uint8_t)(date->dayOfWeek);
+  buf[6] = (uint8_t)(((date->month/10)<<4) | (date->month%10));
+  buf[7] = (uint8_t)(((date->year/10)<<4) | (date->year%10));
+  if (McuPCF85263A_WriteBlock(McuPCF85263A_ADDRESS_TIME_DATE_START_ADDR, buf, sizeof(buf))!=ERR_OK) {
+    return ERR_FAILED;
+  }
+  return ERR_OK;
+}
+
+uint8_t McuPCF85263A_WriteDate(McuPCF85263A_TDATE *date) {
+  uint8_t buf[McuPCF85263A_MEMORY_DATE_SIZE];
+  if (   (date->year>99)
+      || (date->month>12)||(date->month==0)
+      || (date->day>31)||(date->day==0)
+      || (date->dayOfWeek>6)
+     )
+  {
+    return ERR_RANGE;
+  }
+  buf[0] = (uint8_t)(((date->day/10)<<4) | (date->day%10));
+  buf[1] = (uint8_t)(date->dayOfWeek);
+  buf[2] = (uint8_t)(((date->month/10)<<4) | (date->month%10));
+  buf[3] = (uint8_t)(((date->year/10)<<4) | (date->year%10));
+  if (McuPCF85263A_WriteBlock(McuPCF85263A_ADDRESS_DATE_START_ADDR, buf, sizeof(buf))!=ERR_OK) {
+    return ERR_FAILED;
+  }
+  return ERR_OK;
+}
+
+uint8_t McuPCF85263A_WriteTime(McuPCF85263A_TTIME *time) {
+  uint8_t buf[McuPCF85263A_MEMORY_TIME_SIZE];
+
+  if (   ((time->mode==McuPCF85263A_TTIME_MODE_12H)&&((time->hour>12)||(time->hour==0)))
+      || ((time->mode==McuPCF85263A_TTIME_MODE_24H)&&(time->hour>23))
+      || (time->min>59) || (time->sec>59)
+     )
+  {
+    return ERR_RANGE;
+  }
+  buf[0] = (uint8_t)(((time->cs/10)<<4) | (time->cs%10));
+  buf[1] = (uint8_t)(((time->sec/10)<<4) | (time->sec%10));
+  buf[2] = (uint8_t)(((time->min/10)<<4) | (time->min%10));
+  buf[3] = (uint8_t)(((time->hour/10)<<4) | (time->hour%10));
+  if (time->mode==McuPCF85263A_TTIME_MODE_12H) {
+    buf[3] |= (time->am_pm)?(1<<5):0;
+  }
+  if (McuPCF85263A_WriteBlock(McuPCF85263A_ADDRESS_TIME_START_ADDR, buf, sizeof(buf))!=ERR_OK) {
+    return ERR_FAILED;
+  }
+  return ERR_OK;
+}
+
 uint8_t McuPCF85263A_GetTime(TIMEREC *time) {
   McuPCF85263A_TTIME ttime;
   McuPCF85263A_TDATE tdate;
@@ -176,6 +337,72 @@ uint8_t McuPCF85263A_GetTime(TIMEREC *time) {
   time->Min = ttime.min;
   time->Sec = ttime.sec;
   time->Sec100 = ttime.cs;
+  return ERR_OK;
+}
+
+uint8_t McuPCF85263A_SetTimeInfo(uint8_t Hour, uint8_t Min, uint8_t Sec, uint8_t Sec100) {
+  McuPCF85263A_TTIME ttime;
+
+  ttime.hour = Hour;
+  ttime.min = Min;
+  ttime.sec = Sec;
+  ttime.cs = Sec100;
+  ttime.mode = McuPCF85263A_TTIME_MODE_24H;
+  ttime.am_pm = McuPCF85263A_TTIME_AMPM_AM;
+  return McuPCF85263A_WriteTime(&ttime);
+}
+
+uint8_t McuPCF85263A_SetTime(TIMEREC *time) {
+  return McuPCF85263A_SetTimeInfo(time->Hour, time->Min, time->Sec, time->Sec100);
+}
+
+uint8_t McuPCF85263A_GetDate(DATEREC *date) {
+  McuPCF85263A_TTIME ttime;
+  McuPCF85263A_TDATE tdate;
+
+  if (McuPCF85263A_ReadTimeDate(&ttime, &tdate)!=ERR_OK) {
+    return ERR_FAILED;
+  }
+  date->Year = (uint16_t)(tdate.year+2000); /* assume we are after the year 2000 */
+  date->Month = tdate.month;
+  date->Day = tdate.day;
+  return ERR_OK;
+}
+
+uint8_t McuPCF85263A_SetDateInfo(uint16_t Year, uint8_t Month, uint8_t Day) {
+  McuPCF85263A_TDATE tdate;
+
+  if (Year>=2000) {
+    tdate.year = (uint8_t)(Year-2000);
+  } else if (Year>=1900) {
+    tdate.year = (uint8_t)(Year-1900);
+  } else {
+    tdate.year = Year%100;
+  }
+  tdate.month = Month;
+  tdate.day = Day;
+  tdate.dayOfWeek = McuUtility_WeekDay(Year, Month, Day);
+  return McuPCF85263A_WriteDate(&tdate);
+}
+
+uint8_t McuPCF85263A_SetDate(DATEREC *date) {
+  return McuPCF85263A_SetDateInfo(date->Year, date->Month, date->Day);
+}
+
+uint8_t McuPCF85263A_GetTimeDate(TIMEREC *time, DATEREC *date) {
+  McuPCF85263A_TTIME ttime;
+  McuPCF85263A_TDATE tdate;
+
+  if (McuPCF85263A_ReadTimeDate(&ttime, &tdate)!=ERR_OK) {
+    return ERR_FAILED;
+  }
+  time->Hour = ttime.hour;
+  time->Min = ttime.min;
+  time->Sec = ttime.sec;
+  time->Sec100 = ttime.cs;
+  date->Year = (uint16_t)(tdate.year+2000);
+  date->Month = tdate.month;
+  date->Day = tdate.day;
   return ERR_OK;
 }
 
@@ -223,7 +450,50 @@ static uint8_t StrCatHWTimeDate(uint8_t *buf, size_t bufSize) {
   }
   return ERR_OK;
 }
+#pragma endregion TimeDateFunctions
 
+#pragma region ShellCommands
+
+static uint8_t DateCmd(const unsigned char *cmd, McuShell_ConstStdIOType *io) {
+  uint8_t day, month;
+  uint16_t year;
+  uint8_t res = ERR_OK;
+
+  if (McuUtility_ScanDate(&cmd, &day, &month, &year) == ERR_OK) { /* ok, format fine */
+    /* update real time clock */
+    res = McuPCF85263A_SetDateInfo(year, month, day);
+    if (res!=ERR_OK) {
+      McuShell_SendStr((unsigned char*)"*** Failure setting RTC\r\n", io->stdErr);
+      res = ERR_FAILED;
+    }
+  } else {
+    McuShell_SendStr((unsigned char*)"*** Error while reading command! ***", io->stdErr);
+    McuShell_SendStr((void *)cmd, io->stdErr);
+    McuShell_SendStr((unsigned char*)"\r\n", io->stdErr);
+    res = ERR_FAILED;
+  }
+  return res;
+}
+
+static uint8_t TimeCmd(const unsigned char *cmd, McuShell_ConstStdIOType *io) {
+  uint8_t hour, minute, second, hSecond;
+  uint8_t res = ERR_OK;
+
+  if (McuUtility_ScanTime(&cmd, &hour, &minute, &second, &hSecond)==ERR_OK) { /* format fine */
+    /* set RTC time */
+    res = McuPCF85263A_SetTimeInfo(hour, minute, second, hSecond);
+    if (res != ERR_OK) {
+      McuShell_SendStr((unsigned char*)"*** Failure setting RTC time\r\n", io->stdErr);
+      res = ERR_FAILED;
+    }
+  } else {
+    McuShell_SendStr((unsigned char*)"*** Error while reading command: ", io->stdErr);
+    McuShell_SendStr(cmd, io->stdErr);
+    McuShell_SendStr((unsigned char*)"\r\n", io->stdErr);
+    res = ERR_FAILED;
+  }
+  return res;
+}
 
 uint8_t McuPCF85263A_ParseCommand(const unsigned char *cmd, bool *handled, const McuShell_StdIOType *io) {
   int32_t val;
@@ -236,6 +506,8 @@ uint8_t McuPCF85263A_ParseCommand(const unsigned char *cmd, bool *handled, const
     McuShell_SendHelpStr((unsigned char*)"rtc", (const unsigned char*)"Group of PCF85263 RTC commands\r\n", io->stdOut);
     // McuShell_SendHelpStr((unsigned char*)"  help|status", (const unsigned char*)"Print help or status information\r\n", io->stdOut);
     McuShell_SendHelpStr((unsigned char*)"  get datetime", (const unsigned char*)"Print help or status information\r\n", io->stdOut);
+    McuShell_SendHelpStr((unsigned char*)"  set time [hh:mm:ss[,z]]", (const unsigned char*)"Set the current time\r\n", io->stdOut);
+    McuShell_SendHelpStr((unsigned char*)"  set date [dd.mm.yyyy]", (const unsigned char*)"Set the current date\r\n", io->stdOut);
 
     return ERR_OK;
   } else if ((McuUtility_strcmp((char*)cmd, McuShell_CMD_STATUS)==0) || (McuUtility_strcmp((char*)cmd, "rtc get datetime")==0)) {
@@ -248,9 +520,21 @@ uint8_t McuPCF85263A_ParseCommand(const unsigned char *cmd, bool *handled, const
       McuUtility_strcat(buf, sizeof(buf), (unsigned char*)"\r\n");
     }
     McuShell_SendStatusStr((unsigned char*)"  time/date", buf, io->stdOut);
+  } else if (McuUtility_strncmp((char*)cmd, "rtc set date ", sizeof("rtc set date ")-1)==0) {
+    *handled = TRUE;
+    p = cmd + sizeof("rtc set date ")-1;
+    return DateCmd(p, io);
+  } else if (McuUtility_strncmp((char*)cmd, "rtc set time ", sizeof("rtc set time ")-1)==0) {
+    *handled = TRUE;
+    p = cmd + sizeof("rtc set time ")-1;
+    return TimeCmd(p, io);
   }
   return ERR_OK;
 }
+#pragma endregion ShellCommands
+
+
+
 
 
 
